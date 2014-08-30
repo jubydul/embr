284cade80864f492a565de81be9a346c40a3b9c9
Mohammed Jubydul Haque <jubydul.rony@gmail.com>
2014-08-30 14:04:45 +0600

    Todo can inform you now when all Todos are completed

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -40,6 +40,10 @@ Todos.TodosController = Ember.ArrayController.extend({
 
 	completed: function() {
 		return this.filterBy('isCompleted', true).get('length');
+	}.property('@each.isCompleted'),
+
+	allAreDone: function(key, value) {
+	  return !!this.get('length') && this.everyProperty('isCompleted');
 	}.property('@each.isCompleted')
 
 });
