83cd3b7840e46fb2a8dee60fd522141e4d48e425
Mohammed Jubydul Haque <jubydul.rony@gmail.com>
2014-08-30 12:38:38 +0600

    Todo project is at state with a button to remove all completed Todos

--- js/models/todo.js
+++ js/models/todo.js
@@ -1,22 +1,22 @@
 Todos.Todo = DS.Model.extend({
 	title: DS.attr('string'),
-	isCompleted: Ds.attr('bollean');
+	isCompleted: DS.attr('boolean')
 });
 
 Todos.Todo.FIXTURES = [
 	{
 		id: 1,
 		title: 'Learn Ember.js',
-		isComplete: true
+		isCompleted: true
 	},
 	{
 		id: 2,
 		title: '...',
-		isComplete: false
+		isCompleted: false
 	},
 	{
 		id: 3,
 		title: 'Profit!',
-		isComplete: false
+		isCompleted: false
 	}
 ];
